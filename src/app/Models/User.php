1936062b42a3e9d1aef7a843972076d84cc4176d
Michael Ringhus Gertz <michael@ringhus.dk>
2023-11-10 00:16:49 +0100

    Added return type

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
@@ -88,7 +89,7 @@ class User extends Authenticatable
     /**
      * The groups that belong to the user.
      */
-    public function groups()
+    public function groups() : BelongsToMany
     {
         return $this->belongsToMany(Group::class);
     }
