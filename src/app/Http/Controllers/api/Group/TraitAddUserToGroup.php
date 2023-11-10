44e1d4971a06b842284caaddd3b3c37f41739f3c
Michael Ringhus Gertz <michael@ringhus.dk>
2023-11-10 11:43:33 +0100

    Updated method

--- src/app/Http/Controllers/api/Group/TraitAddUserToGroup.php
+++ src/app/Http/Controllers/api/Group/TraitAddUserToGroup.php
@@ -11,41 +11,86 @@ trait TraitAddUserToGroup {
     /**
      * Add a user with a specific email to the group.
      *
-     * @group Groups
-     *
-     * @bodyParam email string required The email of the user to add to the group.
-     *
-     * @response 200 {
-     *     "message": "User added to group successfully"
-     * }
-     *
-     * @response 400 {
-     *     "message": "User with the specified email does not exist"
-     * }
-     *
-     * @response 403 {
-     *     "message": "You are not authorized to add users to this group"
-     * }
+     * @OA\Post(
+     *     path="/api/groups/{id}/add-user",
+     *     tags={"Groups"},
+     *     summary="Add a user to a group",
+     *     description="This endpoint is used to add a user to a group.",
+     *     operationId="addUserToGroup",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="The id of the group",
+     *         @OA\Schema(
+     *             type="integer"
+     *         ),
+     *     ),
+     *     @OA\Parameter(
+     *         name="email",
+     *         in="query",
+     *         required=true,
+     *         description="The email of the user to add to the group",
+     *         @OA\Schema(
+     *             type="string"
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="User added to group successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="User added to group successfully"
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad Request",
+     *         @OA\JsonContent(ref="#/components/schemas/BadRequestError")
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(ref="#/components/schemas/ForbiddenError")
+     *     ),
+     * )
      */
     public function addUserToGroup(Request $request, Group $group)
     {
-        $user = Auth::user();
+        $user = auth('sanctum')->user();
         $emailToAdd = $request->input('email'); // Get the email from the request
 
-        // Check if the user who is adding the member is already in the group
-        if ($group->users()->where('user_id', $user->id)->exists()) {
-            $userToAdd = User::where('email', $emailToAdd)->first();
-
-            // Check if the user with the specified email exists
-            if ($userToAdd) {
-                $group->users()->attach($userToAdd->id);
-
-                return response()->json(['message' => 'User added to group successfully'], 200);
-            } else {
-                return response()->json(['message' => 'User with the specified email does not exist'], 400);
-            }
-        } else {
+        // Check if user whos logged in is member of the group.
+        if (!$group->users()->where('user_id', $user->id)->exists()) {
             return response()->json(['message' => 'You are not authorized to add users to this group'], 403);
         }
+
+        // Check if the email is valid
+        if (!filter_var($emailToAdd, FILTER_VALIDATE_EMAIL)) {
+            return response()->json(['message' => 'The email is not valid'], 400);
+        }
+
+
+        // Check if there is a user with the specified email
+        if (!User::where('email', $emailToAdd)->exists()) {
+            return response()->json(['message' => 'User with the specified email does not exist'], 400);
+        }
+
+
+        // Check if the new user is already member of the group
+        if ($group->users()->where('email', $emailToAdd)->exists()) {
+            return response()->json(['message' => 'User is already member of the group'], 400);
+        }
+
+        // Add the new user to the group
+        $group->users()->attach(User::where('email', $emailToAdd)->first()->id);
+
+        // Return message with success
+        return response()->json(['message' => 'User added to group successfully'], 200);
+
     }
 }
