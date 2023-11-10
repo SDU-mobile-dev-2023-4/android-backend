75191d046cfa9253512a85cb08d054b73e4ef6fb
Michael Ringhus Gertz <michael@ringhus.dk>
2023-11-10 14:00:01 +0100

    redirect user to doc page

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,9 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get("/demo", function () {
-    return Group::all();
+    return redirect("/api/doc");
 });
