899140c3eefd9e555101499dd08aaab4509c2b66
Michael Ringhus Gertz <michael@ringhus.dk>
2023-11-10 15:10:03 +0100

    Added apiDoc

--- src/app/Models/Expense.php
+++ src/app/Models/Expense.php
@@ -6,6 +6,61 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @OA\Schema(
+ *     schema="Expense",
+ *     title="Expense schema",
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Expense id",
+ *         example="1"
+ *     ),
+ *     @OA\Property(
+ *         property="group_id",
+ *         type="integer",
+ *         description="Group id",
+ *         example="1"
+ *     ),
+ *     @OA\Property(
+ *         property="payee_id",
+ *         type="integer",
+ *         description="Payee id",
+ *         example="1"
+ *     ),
+ *     @OA\Property(
+ *         property="created_by",
+ *         type="integer",
+ *         description="Creator id",
+ *         example="1"
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Expense name",
+ *         example="Pizza",
+ *     ),
+ *     @OA\Property(
+ *         property="price",
+ *         type="integer",
+ *         description="Expense price",
+ *         example="100",
+ *     ),
+ *     @OA\Schema(
+ *         schema="updated_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Date and time of last update",
+ *     ),
+ *     @OA\Schema(
+ *         schema="created_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Date and time of creation",
+ *     ),
+ * )
+ */
+
 class Expense extends Model
 {
     use HasApiTokens, HasFactory;
