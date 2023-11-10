be45d591f4173931237b2c0b0821bba1e331756a
Michael Ringhus Gertz <michael@ringhus.dk>
2023-11-10 01:12:38 +0100

    Added cascade delete

--- src/database/migrations/2023_11_09_134636_create_group_user_table.php
+++ src/database/migrations/2023_11_09_134636_create_group_user_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('group_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('group_id')->constrained();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
