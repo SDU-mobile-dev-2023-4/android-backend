14315013be6525fec09fd9daf8172702757ba0f3
Michael Ringhus Gertz <michael@ringhus.dk>
2023-11-10 02:44:27 +0100

    Updated ApiDoc

--- src/app/Http/Controllers/api/Group/TraitIndex.php
+++ src/app/Http/Controllers/api/Group/TraitIndex.php
@@ -9,27 +9,24 @@ trait TraitIndex {
      * return \Illuminate\Http\Response
      *
      * @OA\Get(
-     *   path="/api/groups",
-     *   tags={"Groups"},
-     *   summary="Get all groups",
-     *   description="Get all groups",
-     *
-     *   @OA\Response(
-	 *     response=200,
-	 *     description="OK",
-	 *     @OA\JsonContent(
-	 *        allOf={
-	 *          @OA\Schema(
-	 *            @OA\Property(
-	 *              type="array",
-	 *              @OA\Items(
-	 *                ref="#/components/schemas/Group"
-	 *              )
-	 *            )
-	 *          )
-	 *        }
-	 *      )
-	 *   ),
+     *     path="/api/groups",
+     *     tags={"Groups"},
+     *     summary="Get all groups",
+     *     description="Get all groups",
+     *     operationId="index",
+     *     security={{"bearerAuth":{}}},
+     *     
+     *     @OA\Response(
+	 *         response=200,
+	 *         description="OK",
+	 *         @OA\JsonContent(
+	 *             allOf={
+	 *                 @OA\Schema(
+	 *                     @OA\Property(ref="#/components/schemas/Group")
+	 *                 )
+	 *             }
+	 *         )
+	 *     ),
      * )
      */
     public function index()
